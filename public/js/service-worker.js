280718bc804b59088d863cbd0da66b3dc33a8d59
jillsuarez <jillsuarez@live.com>
2022-02-12 09:47:32 -0500

    created indexDb.js

---
+++ public/js/service-worker.js
@@ -0,0 +1,8 @@
+// const APP_PREFIX = 'BudgetTracker';     
+// const VERSION = 'version_01';
+// const CACHE_NAME = APP_PREFIX + VERSION
+// const FILES_TO_CACHE = [
+//   "./index.html",
+//   "./public/css/styles.css",
+//   "./public/js/index.js",
+// ];
