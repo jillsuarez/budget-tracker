afab262a393ffd28aecbabe4dd7577641b58dffa
jillsuarez <jillsuarez@live.com>
2022-02-12 10:41:02 -0500

    updated service-worker

--- public/js/service-worker.js
+++
@@ -1,8 +0,0 @@
-// const APP_PREFIX = 'BudgetTracker';     
-// const VERSION = 'version_01';
-// const CACHE_NAME = APP_PREFIX + VERSION
-// const FILES_TO_CACHE = [
-//   "./index.html",
-//   "./public/css/styles.css",
-//   "./public/js/index.js",
-// ];
